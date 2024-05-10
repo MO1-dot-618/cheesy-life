0270675e33288212eecf567823c9348673134c23
Malika <malikaoubilla@gmail.com>
2024-05-10 22:49:14 +0100

    database_url env var

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from website import create_app
+import os
 
 app = create_app()
 
 if __name__ == "__main__":
     """ Main Function """
     app.run(host='0.0.0.0', port=5002, debug=True)
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
